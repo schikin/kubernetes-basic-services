5ec6599f3de0d7db5e2368265e7174f704b74786
schikin <sergey.chikin@toptal.com>
2017-08-11 17:28:37 +0300

    initial commit

---
+++ install_autoscaler.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+CLOUD_PROVIDER=aws
+IMAGE=gcr.io/google_containers/cluster-autoscaler:v0.5.4
+MIN_NODES=3
+MAX_NODES=10
+AWS_REGION=eu-west-1
+GROUP_NAME="nodes.spottdev.com"
+SSL_CERT_PATH="/etc/ssl/certs/ca-certificates.crt" # (/etc/ssl/certs for gce)
+
+addon=cluster-autoscaler.yml
+wget -O ${addon} https://raw.githubusercontent.com/kubernetes/kops/master/addons/cluster-autoscaler/v1.6.0.yaml
+
+sed -i -e "s@{{CLOUD_PROVIDER}}@${CLOUD_PROVIDER}@g" "${addon}"
+sed -i -e "s@{{IMAGE}}@${IMAGE}@g" "${addon}"
+sed -i -e "s@{{MIN_NODES}}@${MIN_NODES}@g" "${addon}"
+sed -i -e "s@{{MAX_NODES}}@${MAX_NODES}@g" "${addon}"
+sed -i -e "s@{{GROUP_NAME}}@${GROUP_NAME}@g" "${addon}"
+sed -i -e "s@{{AWS_REGION}}@${AWS_REGION}@g" "${addon}"
+sed -i -e "s@{{SSL_CERT_PATH}}@${SSL_CERT_PATH}@g" "${addon}"
+
+kubectl apply -f ${addon}
